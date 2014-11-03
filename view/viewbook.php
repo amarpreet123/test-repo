3962039d72d3798c7b4636cfb6827be459b1dcca
amarpreet <amarpreet.gmail@buzzyears.com>
2014-11-03 13:04:21 +0530

    looking for a book

---
+++ view/viewbook.php
@@ -0,0 +1,11 @@
+<html>
+<head> </head>
+<body>
+<?php
+echo 'title:'.$book->title.'<br>';
+echo 'author:'.$book->author.'<br>';
+echo 'description:'.$book->description.'<br>';
+?>
+</body>
+</html>
+
