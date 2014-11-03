4d5956238e4112b5a86c210e0efc42bbf6885610
amarpreet <amarpreet.gmail@buzzyears.com>
2014-11-03 13:02:06 +0530

    file having description of book

---
+++ model/book.php
@@ -0,0 +1,15 @@
+<?php
+class book
+{
+public $title;
+public $author;
+public $description;
+
+public function_construct($title)
+{
+$this->title=$title;
+$this->author=$author;
+$this->description=$description;
+}
+}
+?>
