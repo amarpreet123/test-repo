21b514ca5c082869d3b8d69a17579c0d66920a24
amarpreet <amarpreet.gmail@buzzyears.com>
2014-11-03 11:41:32 +0530

    example of associate array

--- app.php
+++ app.php
@@ -1,3 +1,15 @@
 <? php
-echo"hello welcome to our new application being created for ur daily use"
+echo"hello welcome to our new application being created for ur daily use";
+<p id="demo"></p>
+
+<script>
+var person=new Object();
+
+person.name="preeti";
+person.age="20"
+person.lastName="bhatia"
+
+document.getElementId.innerHtml("demo")=person.name + "is" + person.age;
+</script>
+
 ?>
---
+++ associate_array.php
@@ -0,0 +1,9 @@
+<? php
+
+$salaries=array();
+$salaries["preeti"]=2000;
+$salaries["john"]=2000;
+$salaries["jibin"]=2000;
+foreach( $salaries as $employeename = $amount;
+echo  "salaries paid to $employeename is $amount /n";
+?>
